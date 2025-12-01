873f284a046ecc6073c7f651207fa3d352b1841d
fearlesscorpse <baranova.karina4@yandex.ru>
2025-12-01 03:34:33 +0300

    feat: add division

--- karina.php
+++ karina.php
@@ -1,2 +1,7 @@
-$difference = $number1 - $number2;
-echo "## Результат вычитания: **$difference**\n";
+echo "## Результат деления: ";
+if ($number2 != 0) {
+    $result = $number1 / $number2;
+    echo "**$result**\n";
+} else {
+    echo "Деление на ноль невозможно.\n";
+}
