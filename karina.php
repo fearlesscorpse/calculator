d7c313a8a2c67d429e09c457b577b730edae15e1
fearlesscorpse <baranova.karina4@yandex.ru>
2025-12-01 03:28:36 +0300

    feat: add addition

--- karina.php
+++ karina.php
@@ -1,5 +1,2 @@
-$number1 = (float) readline("Введите первое число: ");
-$number2 = (float) readline("Введите второе число: ");
-echo "--- \n";
-echo "Выполняем операции с числами: $number1 и $number2\n";
-echo "--- \n";
+$summa = $number1 + $number2;
+echo "## Результат сложения: **$summa**\n";
