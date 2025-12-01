ef0c2ccfbc52ed3dace1ba5914f8950dda24fa98
fearlesscorpse <baranova.karina4@yandex.ru>
2025-12-01 03:26:18 +0300

    feat: add number input

--- karina.php
+++ karina.php
@@ -1,1 +1,5 @@
-<?php
+$number1 = (float) readline("Введите первое число: ");
+$number2 = (float) readline("Введите второе число: ");
+echo "--- \n";
+echo "Выполняем операции с числами: $number1 и $number2\n";
+echo "--- \n";
